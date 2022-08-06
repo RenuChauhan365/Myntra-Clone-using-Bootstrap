2b665d3dafbf8ca8097529d50435ca2e00ea41c4
Shyam Jaiswal <106074037+shyamjaiswal07@users.noreply.github.com>
2022-08-06 11:55:57 +0530

    code

--- script.js
+++ script.js
@@ -2,13 +2,11 @@
 // Set the date we're counting down to
 var countDownDate = new Date("sep 5,2022  15:37:25").getTime();
 
-// Update the count down every 1 second
 var x = setInterval(function() {
 
   // Get today's date and time
   var now = new Date().getTime();
-    
-  // Find the distance between now and the count down date
+
   var distance = countDownDate - now;
     
   // Time calculations for days, hours, minutes and seconds
@@ -20,10 +18,9 @@ var x = setInterval(function() {
   // Output the result in an element with id="demo"
   document.getElementById("countDown").innerHTML =  hours + "h "
   + minutes + "m " + seconds + "s ";
-    
-  // If the count down is over, write some text 
+
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+    document.getElementById("countDown").innerHTML = "EXPIRED";
   }
 }, 1000);
