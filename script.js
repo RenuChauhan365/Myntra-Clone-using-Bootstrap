9e549c66139640feb37b817ce6dfe32c11ef60b6
Shyam Jaiswal <106074037+shyamjaiswal07@users.noreply.github.com>
2022-08-09 16:08:07 +0530

    Working on cart page

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Set the date we're counting down to
+
 var countDownDate = new Date("sep 5,2022  15:37:25").getTime();
 
 var x = setInterval(function () {
@@ -6,9 +7,9 @@ var x = setInterval(function () {
   var now = new Date().getTime();
 
   var distance = countDownDate - now;
-
+  
   // Time calculations for days, hours, minutes and seconds
-
+  
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
@@ -26,6 +27,7 @@ var x = setInterval(function () {
 // ====>
 // Adding in cart =====>
 let shoping_cart = [];
+
 let jsondata = {
   data: [
     {
@@ -199,44 +201,44 @@ jsondata.data.forEach((item, index) => {
   return (all_data += `
   
   <div style="margin-bottom:25px" class="tshirt_items">
-    <div class="t-shirt_image">
+  <div class="t-shirt_image">
   
-        <img 
-          src="${item.url}"
-          alt=""
-        />
-        <div id="layer">
-        <button onClick="addToCart(${index})" type="button" id="liveToastBtn" class="btn btn-outline-light">Add to Cart</button>
-        </div>
-      </div>
-      <div class="t_shirt_discription">
-        <div class="t-shirt_name">
-        <span style="display:none">${index + 1}</span>
-        
-          <h6 style="font-weight: 900">${item.Tname}</h6>
-        </div>
-        <div class="t-shirt_Discription">
-          <p>${item.description}</p>
+  <img 
+  src="${item.url}"
+  alt=""
+  />
+  <div id="layer">
+  <button onClick="addToCart(${index})" type="button" id="liveToastBtn" class="btn btn-outline-light">Add to Cart</button>
+  </div>
+  </div>
+  <div class="t_shirt_discription">
+  <div class="t-shirt_name">
+  <span style="display:none">${index + 1}</span>
+  
+  <h6 style="font-weight: 900">${item.Tname}</h6>
+  </div>
+  <div class="t-shirt_Discription">
+  <p>${item.description}</p>
         </div>
         <div class="t-shirt_price">
-          <span><strong>Rs ${item.price}</strong></span>
-          <span><strike>Rs 700</strike></span>
-          <span style="color: red">(40% off)</span>
-    </div>
-  </div>
-  </div>
-`);
-});
+        <span><strong>Rs ${item.price}</strong></span>
+        <span><strike>Rs 700</strike></span>
+        <span style="color: red">(40% off)</span>
+        </div>
+        </div>
+        </div>
+        `);
+      });
 
 
-
-// console.log(all_data);
-document.getElementById("t-shirt").innerHTML = all_data;
-
-const toastTrigger = document.getElementById('liveToastBtn')
-const toastLiveExample = document.getElementById('liveToast')
-function addToCart(index) {
-  // console.log(index);
+ document.getElementById("t-shirt").innerHTML = all_data;
+      
+      // console.log(all_data);
+      
+      const toastTrigger = document.getElementById('liveToastBtn')
+      const toastLiveExample = document.getElementById('liveToast')
+      function addToCart(index) {
+        // console.log(index);
   shoping_cart.push(jsondata.data[index]);
   addToCartt();
 
@@ -251,58 +253,55 @@ function addToCart(index) {
 
 let cart_data = "";
 function addToCartt() {
-  console.log(cart_data);
+  
   shoping_cart.forEach((data, index) => {
     cart_data += `<div class="flex_Cart_item">
    <div class="t-shirt-image">
   <img
-    src="${data.url}"
+  src="${data.url}"
     alt=""
-  />
-</div>
-<div style="margin: 20px 0" class="flex_align">
+    />
+    </div>
+    <div style="margin: 20px 0" class="flex_align">
   <div class="t-shirt_containt">
-    <h4>${data.Tname}</h4>
-    <p style="margin-bottom: 5px">${data.description}</p>
+  <h4>${data.Tname}</h4>
+  <p style="margin-bottom: 5px">${data.description}</p>
   </div>
   <div class="rating_button">
-    <button>4 | 9.8k Ratings</button>
+  <button>4 | 9.8k Ratings</button>
   </div>
   <hr />
   <div class="t-shirt_price">
-    <span><strong>${data.price}</strong></span>
-    <span><strike>Rs 700</strike></span>
-    <span style="color: red">(40% off)</span>
+  <span><strong>${data.price}</strong></span>
+  <span><strike>Rs 700</strike></span>
+  <span style="color: red">(40% off)</span>
     <p style="color: green; margin-bottom: 10px">
-      <strong>inclusive of all taxes</strong>
+    <strong>inclusive of all taxes</strong>
     </p>
-  </div>
-  <div class="size_chart">
+    </div>
+    <div class="size_chart">
     <span style="margin: 10px 10px 10px 0"
-      ><strong>SELECT SIZE</strong></span
+    ><strong>SELECT SIZE</strong></span
     >
     <span style="color: red"><strong>SIZE CHART </strong></span>
     <div style="margin: 0 0" class="size">
-      <button type="button">S</button>
-      <button type="button">M</button>
-      <button type="button">L</button>
-      <button type="button">XL</button>
-      <button type="button">XXL</button>
+    <button type="button">S</button>
+    <button type="button">M</button>
+    <button type="button">L</button>
+    <button type="button">XL</button>
+    <button type="button">XXL</button>
     </div>
-  </div>
-  <div class="add_to_bag">
+    </div>
+    <div class="add_to_bag">
     <button onClick="addToBag(${index})"><img src="./image/bag.svg" alt="" /> Add To Bag</button>
     <button>
-      <img src="./image/heart.svg" alt="" /> Add To WishList
+    <img src="./image/heart.svg" alt="" /> Add To WishList
     </button>
-  </div>
-</div>
-</div>
-<hr>`;
+    </div>
+    </div>
+    </div>
+    <hr>`;
   });
-  console.log(cart_data);
-  // document.getElementById("cart_Box").innerHTML = "hfghfh";=+
+
   localStorage.setItem("myData", cart_data);
 }
-// document.querySelector("#cart_Box").innerHTML = localStorage.getItem("FirstName");
-// document.querySelector(".clickedMe")
