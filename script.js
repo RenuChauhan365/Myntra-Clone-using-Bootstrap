5d3eb2b9fe48e1663c8e75d2c41c71f35271e365
Shyam Jaiswal <106074037+shyamjaiswal07@users.noreply.github.com>
2022-08-11 12:08:10 +0530

    ............

--- script.js
+++ script.js
@@ -3,18 +3,12 @@
 var countDownDate = new Date("sep 5,2022  15:37:25").getTime();
 
 var x = setInterval(function () {
-  // Get today's date and time
   var now = new Date().getTime();
-
   var distance = countDownDate - now;
-
-  // Time calculations for days, hours, minutes and seconds
-
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Output the result in an element with id="demo"
   document.getElementById("countDown").innerText =
     hours + "h " + minutes + "m " + seconds + "s ";
 
@@ -24,7 +18,7 @@ var x = setInterval(function () {
   }
 }, 1000);
 
-// ====>
+
 // Adding in cart =====>
 let shoping_cart = [];
 
